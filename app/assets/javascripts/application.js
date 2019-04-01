8c72f9c193775b7f1edb5a8a811d3bb695fc3c7c
Lena Li <lenal@andrew.cmu.edu>
2019-04-01 22:31:11 +0000

    front end looking good

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,20 @@
 //= require rails-ujs
-//= require jquery
 //= require materialize-sprockets
 //= require_tree .
 
 $('select').material_select();
 
 $( document ).ready(function () { 
-    $(".dropdown-button").dropdown(); 
+    $(".dropdown-button").dropdown({ hover: true, constrainWidth: false }); 
+
 
 
     $('.datepicker').pickadate({
         format: 'mmmmdd,yyyy',
         formatSubmit: 'mmmmdd,yyyy',
-        selectMonths: true, // Creates a dropdown to control month selectYears: 15, // Creates a dropdown of 15 years to control year, today: 'Today',
+        selectMonths: true, // Creates a dropdown to control month 
+        changeYear: true, // Creates a dropdown of 15 years to control year, today: 'Today',
+        yearRange:"1980:2019",
         clear: 'Clear',
         close: 'Ok',
         closeOnSelect: false // Close upon selecting a date,
